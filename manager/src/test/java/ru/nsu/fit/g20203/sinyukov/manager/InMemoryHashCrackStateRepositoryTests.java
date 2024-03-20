16186fb1af559e2b27b3c1774143f3597592fed3
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2024-03-20 16:58:13 +0700

    [lab1, manager]: InMemoryHashCrackStateRepository test

---
+++ manager/src/test/java/ru/nsu/fit/g20203/sinyukov/manager/InMemoryHashCrackStateRepositoryTests.java
@@ -0,0 +1,23 @@
+package ru.nsu.fit.g20203.sinyukov.manager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.nsu.fit.g20203.sinyukov.manager.hashcrackstaterepository.InMemoryHashCrackStateRepository;
+
+import java.util.UUID;
+
+public class InMemoryHashCrackStateRepositoryTests {
+
+    @Test
+    void givenCreateCommand_whenGetHashCrack_thenReturnNewHashCrackByGivenId() {
+        final var hashCrackStateRepository = new InMemoryHashCrackStateRepository();
+        final var id = UUID.randomUUID();
+
+        final HashCrackState expectedHashCrackState = new HashCrackState();
+
+        hashCrackStateRepository.createNewHashCrack(id);
+
+        Assertions.assertTrue(hashCrackStateRepository.containsId(id));
+        Assertions.assertEquals(expectedHashCrackState, hashCrackStateRepository.getHashCrack(id));
+    }
+}
