20a35f9a86e309e3dcd866ab2606aa72f3ce9168
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2024-04-12 10:35:38 +0700

    [lab2, mongo]: client settings and connection string from properties

--- manager/src/main/java/ru/nsu/fit/g20203/sinyukov/manager/MongoDBHashConfig.java
+++ manager/src/main/java/ru/nsu/fit/g20203/sinyukov/manager/MongoDBHashConfig.java
@@ -1,9 +1,9 @@
 package ru.nsu.fit.g20203.sinyukov.manager;
 
-import com.mongodb.MongoClientSettings;
+import com.mongodb.*;
 import com.mongodb.client.MongoClient;
 import org.bson.UuidRepresentation;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -19,22 +19,15 @@ public class MongoDBHashConfig {
 
     private final static String databaseName = "hashCrackDB";
 
-    private final int port;
-    private final String host;
-
-    public MongoDBHashConfig(@Value("${spring.mongodb.port}") int port,
-                             @Value("${spring.mongodb.host}") String host) {
-        this.port = port;
-        this.host = host;
-    }
-
     @Bean
-    public MongoClientFactoryBean mongoClientFactory() {
+    public MongoClientFactoryBean mongoClientFactory(MongoProperties mongoProperties) {
         MongoClientFactoryBean mongo = new MongoClientFactoryBean();
-        mongo.setHost(host);
-        mongo.setPort(port);
+        mongo.setConnectionString(new ConnectionString(mongoProperties.getUri()));
         final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .uuidRepresentation(UuidRepresentation.STANDARD)
+                .writeConcern(WriteConcern.MAJORITY)
+                .readConcern(ReadConcern.MAJORITY)
+                .readPreference(ReadPreference.primaryPreferred())
                 .build();
         mongo.setMongoClientSettings(mongoClientSettings);
         return mongo;
